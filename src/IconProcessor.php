822aa1185935c8462ebc3a3db33b08b09af9fce1
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-06-30 21:16:06 +0530

    wip

--- src/IconProcessor.php
+++ src/IconProcessor.php
@@ -2,17 +2,113 @@
 
 namespace Codeat3\BladeIconGeneration;
 
-use PhpCsFixer\Finder;
+use DOMDocument;
 use Illuminate\Support\Str;
 
 class IconProcessor
 {
     protected $file;
+    protected $filepath;
+    protected $svgDoc;
+    protected $svgLine;
+    protected $config;
+
+    protected $attributesToRemove = [
+        'width',
+        'height',
+        'class',
+        'style',
+    ];
 
     public function __construct($filepath, $config = [])
     {
+        $this->filepath = $filepath;
         $this->file = new \SplFileInfo($filepath);
+
+        $this->config = $config;
+
+        $this->svgDoc = new DOMDocument();
+        $this->svgDoc->formatOutput = false;
+        $this->svgDoc->load($filepath);
     }
+
+    public function save()
+    {
+        $destinationPath = $this->getDestinationFilePath();
+
+        if ($this->filepath !== $destinationPath) {
+            rename($this->filepath, $destinationPath);
+        }
+
+        $this->svgLine = str_replace(PHP_EOL, '', $this->svgLine);
+        file_put_contents($destinationPath, $this->svgLine);
+    }
+
+    public function preOptimization()
+    {
+        return $this;
+    }
+
+    public function postOptimization()
+    {
+        return $this;
+    }
+
+    private function optimizeOutlineSvg(&$svgEL)
+    {
+        if ($svgEL->getAttribute('fill') !== 'none') {
+            $svgEL->setAttribute('fill', 'none');
+        }
+
+        if ($svgEL->getAttribute('stroke') !== 'currentColor') {
+            $svgEL->setAttribute('stroke', 'currentColor');
+        }
+    }
+
+    private function optimizeSolidSvg(&$svgEL)
+    {
+        if ($svgEL->getAttribute('fill') !== 'currentColor') {
+            $svgEL->setAttribute('fill', 'currentColor');
+        }
+    }
+
+    private function addCustomAttributes(&$svgEL, $attributes = [])
+    {
+        foreach ($attributes as $key => $value) {
+            $svgEL->setAttribute($key, $value);
+        }
+    }
+
+    public function optimize()
+    {
+        // remove unwanted attributes id, class, width, height
+        $svgEL = $this->svgDoc->getElementsByTagName('svg')[0];
+        foreach ($this->attributesToRemove as $attribute) {
+            $svgEL->removeAttribute($attribute);
+        }
+
+        if ($this->config['is-outline'] ?? false) {
+            $this->optimizeOutlineSvg($svgEL);
+        }
+
+        if ($this->config['is-solid'] ?? false) {
+            $this->optimizeSolidSvg($svgEL);
+        }
+
+        if (
+            $this->config['custom-attributes'] ?? false
+            && is_array($this->config['custom-attributes'])
+        ) {
+            $this->addCustomAttributes($svgEL, $this->config['custom-attributes']);
+        }
+
+        return $this;
+    }
+
+    protected function setSvgAsString() {
+        $this->svgDoc = $this->svgDoc->saveXML();
+    }
+
     protected function getDestinationPath($dir = ''): string
     {
         $str = $this->file->getPath();
@@ -28,6 +124,11 @@ class IconProcessor
         return $this->file->getExtension();
     }
 
+    protected function getFileName()
+    {
+        return $this->file->getBasename('.svg');
+    }
+
     public function getDestinationFilePath($suffix = '', $dir = '')
     {
         return $this->getDestinationPath($dir)
@@ -35,10 +136,5 @@ class IconProcessor
                 .$suffix
                 .'.'
                 .$this->getExtension();
-    }
-
-    protected function getFileName()
-    {
-        return $this->file->getBasename('.svg');
     }
 }
